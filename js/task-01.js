214c2aa9829e3c7c204d6c5c0ff3c9350ad01a3b
Igor Lunyakin <lunyaigor484@gmail.com>
2023-07-08 21:47:36 +0300

    fixed homework 6

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,12 @@
 const itemsEl = document.querySelectorAll('.item');
 
-let countOfCategories = 0;
-
-itemsEl.forEach(() => {
-    countOfCategories += 1;
-});
-
-console.log(`Number of categories: ${countOfCategories}`)
+console.log(`Number of categories: ${itemsEl.length}`)
 
 itemsEl.forEach(item => {
-    console.log(item.firstElementChild.textContent);
+    console.log(`Category: ${item.firstElementChild.textContent}`);
     const itemsOfCategorie = item.querySelectorAll('li');
-    let countOfItems = 0;
 
-    itemsOfCategorie.forEach(() => {
-        countOfItems += 1;
-    });
-
-    console.log(`Elements: ${countOfItems}`);
+    console.log(`Elements: ${itemsOfCategorie.length}`);
 })
 
 
